c49f6b128406ccf097aed7973b22e86c47331fc6
Juan Salvatore <juansalvatore@live.com.ar>
2018-01-12 17:02:49 -0300

    End simple animation practice

--- simple_animations/js/app.js
+++ simple_animations/js/app.js
@@ -4,5 +4,32 @@ document.addEventListener('DOMContentLoaded', event => {
 
 startTween = () => {
   const toolBox = document.getElementById('toolBox')
-  TweenLite.to(toolBox, 1, { x: 280, y: 230 })
+
+  // toolbox animation
+  TweenLite.to(toolBox, 1, {
+    delay: 0.4,
+    x: 280,
+    y: 230,
+    scale: 2,
+    onComplete: returnToNormal,
+    onCompleteParams: [toolBox],
+  })
+
+  // text animation
+  TweenLite.to('.asapReg', 1, {
+    delay: 0.4,
+    color: '#ff0000',
+    fontSize: '+=75',
+    onComplete: returnTextToNormal,
+  })
+}
+
+// return functions
+returnToNormal = obj => {
+  obj ? TweenLite.to(obj, 1, { delay: 1, x: 0, y: 0, scale: 1 }) : null
+}
+
+returnTextToNormal = () => {
+  const toolBox = document.getElementById
+  TweenLite.to('.asapReg', 1, { color: '#fff', fontSize: '-=75' })
 }
